bfcf173f6264d2928cd14da7b751e01712652609
Miguel Amores <migueamores@gmail.com>
2024-04-26 08:23:48 -0500

    add ibm font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const IBMPlex = IBM_Plex_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-ibm-plex",
+  fallback: ["Helvetica", "Arial", "sans-serif"],
+});
 
 export const metadata: Metadata = {
   title: "Image Wizard",
@@ -16,7 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+        {children}
+      </body>
     </html>
   );
 }
