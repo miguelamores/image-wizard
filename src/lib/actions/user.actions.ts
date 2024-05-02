1886821f2f65813b8ecb5f0cde264733cc41cc26
Miguel Amores <migueamores@gmail.com>
2024-05-02 14:17:08 -0500

    create user actions

---
+++ src/lib/actions/user.actions.ts
@@ -0,0 +1,60 @@
+import User from "../database/models/user.model";
+import { connectToDatabase } from "../database/mongoose";
+import { handleError } from "../utils";
+
+export async function createUser(user: CreateUserParams) {
+  try {
+    await connectToDatabase();
+
+    const newUser = await User.create(user);
+
+    return JSON.parse(JSON.stringify(newUser));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+export async function getUserById(userId: string) {
+  try {
+    await connectToDatabase();
+
+    const user = await User.findOne({ clerkId: userId });
+
+    if (!user) throw new Error("User not found");
+
+    return JSON.parse(JSON.stringify(user));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+export async function updateUser(clerkId: string, user: UpdateUserParams) {
+  try {
+    await connectToDatabase();
+
+    const updatedUser = await User.findOneAndUpdate({ clerkId }, user, {
+      new: true,
+    });
+
+    if (!updateUser) throw new Error("User update failed");
+
+    return JSON.parse(JSON.stringify(updatedUser));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+export async function deleteUser(clerkId: string) {
+  try {
+    await connectToDatabase();
+    const userToDelete = await User.findOne({ clerkId });
+
+    if (!userToDelete) throw new Error("User not found");
+
+    const deletedUser = await User.findOneAndDelete(userToDelete._id);
+
+    return deleteUser ? JSON.parse(JSON.stringify(deleteUser)) : null;
+  } catch (error) {
+    handleError(error);
+  }
+}
