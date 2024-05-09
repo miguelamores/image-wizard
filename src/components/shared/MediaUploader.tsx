cc477746d48810084db20e0af82039fbe4e9c5ce
Miguel Amores <migueamores@gmail.com>
2024-05-09 15:12:18 -0500

    create uploader using cloudinary widget

---
+++ src/components/shared/MediaUploader.tsx
@@ -0,0 +1,63 @@
+import { useToast } from "@/components/ui/use-toast";
+import { CldUploadWidget } from "next-cloudinary";
+
+type MediaUploaderProps = {
+  onValueChange: (value: string) => void;
+  setImage: React.Dispatch<any>;
+  image: any;
+  publicId: string;
+  type: string;
+};
+
+const MediaUploader = ({
+  onValueChange,
+  setImage,
+  image,
+  publicId,
+  type,
+}: MediaUploaderProps) => {
+  const { toast } = useToast();
+
+  const onUploadSuccessHandler = (result: any) => {
+    toast({
+      title: "Image uploaded successfully",
+      description: "1 credit was deducted from your account",
+      duration: 5000,
+      className: "success-toast",
+    });
+  };
+
+  const onUploadErrorHandler = () => {
+    toast({
+      title: "Something went wrong while uploading",
+      description: "Please try again",
+      duration: 5000,
+      className: "error-toast",
+    });
+  };
+
+  return (
+    <CldUploadWidget
+      uploadPreset="jsm_image_wizard"
+      options={{
+        multiple: false,
+        resourceType: "image",
+      }}
+      onSuccess={onUploadSuccessHandler}
+      onError={onUploadErrorHandler}
+    >
+      {({ open }) => (
+        <div className="flex flex-col gap-4">
+          <h3 className="h3-bold text-dark-600">Original</h3>
+          {publicId ? (
+            <>HERE IS THE IMAGE</>
+          ) : (
+            <div className="">HERE IS NO IMAGE</div>
+          )}
+        </div>
+      )}
+    </CldUploadWidget>
+  );
+};
+
+export default MediaUploader;
