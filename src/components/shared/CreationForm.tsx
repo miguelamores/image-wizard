938b8479e1039f608b5089ea66385e1bf50a5189
Miguel Amores <migueamores@gmail.com>
2025-03-05 21:31:38 -0500

    fix ts errors

--- src/components/shared/CreationForm.tsx
+++ src/components/shared/CreationForm.tsx
@@ -30,8 +30,8 @@ export const schema = z.object({
 
 type ImageObjType = {
   url: string;
-  height: number;
-  width: number;
+  height?: number | undefined;
+  width?: number | undefined;
 } | null;
 
 interface Props {
